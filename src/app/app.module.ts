957c5774b90f300ee5b6b3b1b96a1a4cb520719f
thiago_osim <thiago_osim@hotmail.com>
2026-02-24 22:53:02 -0300

    Criado a diretiva card-header.directive.ts e utilizado para projeção de conteúdo no ng-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { CardHeaderDirective } from './card/directives/card-header.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    CardHeaderDirective
   ],
   imports: [
     BrowserModule
