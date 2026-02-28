fe85b41b9e3e9200fc17a4a1d67b93a00031f2aa
thiago_osim <thiago_osim@hotmail.com>
2026-02-28 12:04:08 -0300

    Alterado estilização do layout e criado diretiva card-main-text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { CardHeaderDirective } from './card/directives/base/card-header.directive';
 import { CardContentDirective } from './card/directives/base/card-content.directive';
+import { CardMainTextDirective } from './card/directives/texts/card-main-text.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
     CardHeaderDirective,
-    CardContentDirective
+    CardContentDirective,
+    CardMainTextDirective
   ],
   imports: [
     BrowserModule
---
+++ src/app/card/directives/texts/card-main-text.directive.spec.ts
@@ -0,0 +1,8 @@
+import { CardMainTextDirective } from './card-main-text.directive';
+
+describe('CardMainTextDirective', () => {
+  it('should create an instance', () => {
+    const directive = new CardMainTextDirective();
+    expect(directive).toBeTruthy();
+  });
+});
